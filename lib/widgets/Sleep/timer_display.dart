fcdc7c849e3e66e6e3dbafb3a32c1f058e621791
Punn Kanploy <pkanploy@gmail.com>
2024-10-04 22:17:07 +0700

    Update timer_display

--- lib/widgets/Sleep/timer_display.dart
+++ lib/widgets/Sleep/timer_display.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class TimerDisplay extends StatelessWidget {
+class TimerDisplay extends StatefulWidget {
   const TimerDisplay({Key? key}) : super(key: key);
 
   @override
+  _TimerDisplayState createState() => _TimerDisplayState();
+}
+
+class _TimerDisplayState extends State<TimerDisplay> {
+  late String _currentTime;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTime();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      _updateTime();
+    });
+  }
+
+  void _updateTime() {
+    final now = DateTime.now();
+    setState(() {
+      _currentTime = "${_formatTwoDigits(now.hour)}:${_formatTwoDigits(now.minute)}";
+    });
+  }
+
+  String _formatTwoDigits(int number) {
+    return number.toString().padLeft(2, '0');
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Text(
-        '20:30',
-        style: TextStyle(
+        _currentTime,
+        style: const TextStyle(
           color: Colors.white,
           fontSize: 72,
           fontWeight: FontWeight.bold,
